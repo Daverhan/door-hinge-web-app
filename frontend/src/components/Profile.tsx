b5c580023ac60ddbe6ab63b363c33d170e409fab
David Silva <daverhan02@gmail.com>
2024-01-27 15:15:58 -0500

    Add fetch call for a user's profile data

--- frontend/src/components/Profile.tsx
+++ frontend/src/components/Profile.tsx
@@ -1,7 +1,28 @@
+import { useState, useEffect } from "react";
+
 function Profile() {
+  const [data, setData] = useState({
+    first_name: "",
+    last_name: "",
+    id: "",
+  });
+
+  useEffect(() => {
+    fetch("/api/users/1")
+      .then((res) => res.json())
+      .then((jsonData) => {
+        setData(jsonData);
+      });
+  }, []);
+
   return (
-    <section>
-      <h1>test</h1>
+    <section className="bg-blue-100 h-screen pt-16">
+      <div className="font-semibold text-xl m-2">
+        <p>
+          Name: {data.first_name} {data.last_name}
+        </p>
+        <p>ID: {data.id}</p>
+      </div>
     </section>
   );
 }
