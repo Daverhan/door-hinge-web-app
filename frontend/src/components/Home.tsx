4586e77f1095b596a405cead8df87cf998c3f5f0
David Silva <daverhan02@gmail.com>
2024-01-26 20:02:04 -0500

    Modify Navbar

--- frontend/src/components/Home.tsx
+++ frontend/src/components/Home.tsx
@@ -1,5 +1,19 @@
 function Home() {
-  return <section className="bg-blue-100 h-screen"></section>;
+  return (
+    <section className="bg-blue-100 h-screen pt-16">
+      <div className="flex flex-col bg-color-200">
+        <div className="flex items-center justify-center h-64 bg-red-300">
+          <h2 className="text-2xl">block 1</h2>
+        </div>
+        <div className="flex items-center justify-center h-64 bg-green-300">
+          <h2 className="text-2xl">block 2</h2>
+        </div>
+        <div className="flex items-center justify-center h-64 bg-orange-300">
+          <h2 className="text-2xl">block 3</h2>
+        </div>
+      </div>
+    </section>
+  );
 }
 
 export default Home;
--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,17 +1,58 @@
+import { useState, useEffect } from "react";
 import { RxHamburgerMenu } from "react-icons/rx";
+import { IoClose } from "react-icons/io5";
 
 function Navbar() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
+  useEffect(() => {
+    const handleResize = () => {
+      if (window.innerWidth >= 1024) {
+        setIsOpen(false);
+      }
+    };
+
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
   return (
-    <section className="grid grid-cols-[30%_40%_30%] h-16 w-screen">
+    <section className="grid grid-cols-[30%_40%_30%] h-16 bg-white fixed w-full top-0">
       <div></div>
+
       <div className="m-auto">
         <h1 className="font-bold text-3xl">DoorHinge</h1>
       </div>
-      <div className="text-4xl m-auto">
-        <div className="lg:hidden">
-          <RxHamburgerMenu />
+
+      <div className="h-full text-4xl">
+        <div className="lg:hidden flex justify-end items-center w-full h-full pr-5">
+          {isOpen ? (
+            <IoClose onClick={toggleMenu}></IoClose>
+          ) : (
+            <RxHamburgerMenu onClick={toggleMenu}></RxHamburgerMenu>
+          )}
         </div>
-        <div className="hidden lg:flex gap-5 lg:text-xl">
+
+        {/* Mobile Menu */}
+        <div
+          className={`lg:hidden text-3xl fixed top-16 bottom-0 left-0 bg-blue-100 w-64 z-10 transition-transform transform ${
+            isOpen ? "translate-x-0" : "-translate-x-full"
+          }`}
+        >
+          <p className="p-4">Profile</p>
+          <p className="p-4">Favorites</p>
+          <p className="p-4">Messages</p>
+        </div>
+
+        {/* Desktop Menu */}
+        <div className="hidden lg:flex lg:justify-evenly lg:items-center lg:h-full lg:text-xl">
           <p>Messages</p>
           <p>Favorites</p>
           <p>Profile</p>
