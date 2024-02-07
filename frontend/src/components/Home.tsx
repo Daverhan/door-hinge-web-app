62c595495a5492fb2aa47f3abe775a8c83285d6f
David Silva <daverhan02@gmail.com>
2024-02-07 16:16:06 -0500

    Add image carousel

--- frontend/src/components/Home.tsx
+++ frontend/src/components/Home.tsx
@@ -1,15 +1,55 @@
+import { Carousel } from "@material-tailwind/react";
+import Home_1 from "../assets/1.jpg";
+import Home_2 from "../assets/2.jpg";
+import Home_3 from "../assets/3.jpg";
+
 function Home() {
   return (
     <section className="bg-blue-100 h-screen pt-16">
-      <div className="flex flex-col bg-color-200">
-        <div className="flex items-center justify-center h-64 bg-red-300">
-          <h2 className="text-2xl">block 1</h2>
+      <div className="flex flex-col lg:grid lg:grid-cols-[25%_50%_25%] h-full">
+        <div className="hidden lg:flex h-full w-full justify-center items-center">
+          <p className="break-words mx-4">
+            This section can be used to filter for properties
+          </p>
         </div>
-        <div className="flex items-center justify-center h-64 bg-green-300">
-          <h2 className="text-2xl">block 2</h2>
+        <div className="grid grid-rows-[52.5%_32.5%_15%] lg:grid-rows-[62.5%_27.5%_10%] h-full">
+          <div>
+            <Carousel
+              className="z-0"
+              placeholder={<p className="text-3xl">Loading...</p>}
+            >
+              <img
+                src={Home_1}
+                alt="image 1"
+                className="h-full w-full object-cover"
+              />
+              <img
+                src={Home_2}
+                alt="image 2"
+                className="h-full w-full object-cover"
+              />
+              <img
+                src={Home_3}
+                alt="image 3"
+                className="h-full w-full object-cover"
+              />
+            </Carousel>
+          </div>
+          <div className="flex flex-col bg-green-300 pl-2 pt-2 items-left overflow-hidden break-words">
+            <p>Lister's Name: David Silva</p>
+            <p>Listing's Price: $350,000</p>
+            <br></br>
+            <p>4BED/2BATH LOVELY HOME</p>
+          </div>
+          <div className="flex justify-evenly items-center bg-orange-300">
+            <p className="text-3xl">No</p>
+            <p className="text-3xl">Yes</p>
+          </div>
         </div>
-        <div className="flex items-center justify-center h-64 bg-orange-300">
-          <h2 className="text-2xl">block 3</h2>
+        <div className="hidden lg:flex h-full w-full justify-center items-center">
+          <p className="break-words mx-4">
+            This section can be used to directly message the lister
+          </p>
         </div>
       </div>
     </section>
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,8 +1,13 @@
-/** @type {import('tailwindcss').Config} */
-export default {
+const withMT = require("@material-tailwind/react/utils/withMT");
+
+module.exports = withMT({
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      home_1: "url('/src/assets/1.jpg')",
+      home_2: "url('/src/assets/2.jpg')",
+      home_3: "url('/src/assets/3.jpg')",
+    },
   },
   plugins: [],
-};
+});
