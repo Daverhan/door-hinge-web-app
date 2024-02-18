40e839d99aa259ce85513a7e6ebc83c640ad5028
Aidan Collins <acc22j@fsu.edu>
2024-02-18 18:42:17 -0500

    Start database logic

--- backend/app/models/user.py
+++ backend/app/models/user.py
@@ -5,10 +5,107 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     first_name = db.Column(db.String(50), nullable=False)
     last_name = db.Column(db.String(50), nullable=False)
+    email = db.Column(db.String(50), nullable=False)
+    username = db.Column(db.String(50), nullable=False)
+    password = db.Column(db.String(50), nullable=False)
 
     def to_dict(self):
         return {
             'id': self.id,
             'first_name': self.first_name,
             'last_name': self.last_name,
+            'email': self.email,
+            'username': self.username,
+            'password': self.password
+        }
+
+
+class Listing(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    lister_id = db.Column(db.Integer, nullable=False)
+    name = db.Column(db.String(50), nullable=False)
+    desc = db.Column(db.String(500), nullable=False)
+    price = db.Column(db.Integer, nullable=False)
+    num_beds = db.Column(db.Integer, nullable=False)
+    num_baths = db.Column(db.Integer, nullable=False)
+    sqft = db.Column(db.Integer, nullable=False)
+    addr_id = db.relationship(
+            'Address', backref='Listing', lazy=True, cascade="all, delete-orphan")
+    image_id = db.relationship(
+            'Image', backref='Listing', lazy=True, cascade="all, delete-orphan")
+
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'lister_id': self.lister_id,
+            'desc': self.desc,
+            'price': self.price,
+            'num_beds': self.num_beds,
+            'num_baths': self.num_baths,
+            'sqft': self.sqft,
+            'addr_id': [Address.to_dict() for Address in self.addr_id],
+            'image_id': [Images.to_dict() for Images in self.image_id]
+        }
+
+
+class Messages(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(500), nullable=False)
+    timestamp = db.Column(db.DateTime, nullable=False)
+    chat_id = db.Column(db.Integer, db.ForeignKey('Chat.id', nullable=False))
+
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'content': self.content,
+            'timestamp': self.timestamp,
+            'chat_id': self.chat_id
+        }
+
+
+class Chat(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    messages = db.relationship(
+            'Message', backref='Chat', lazy=True)
+
+    def to_dict(self):
+        return {
+                'id': self.id,
+                'messages': [Message.to_dict() for Message in self.messages]
+        }
+
+
+class Address(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    house_num = db.Column(db.Integer, nullable=False)
+    street_name = db.Column(db.String(200), nullable=False)
+    city = db.Column(db.String(50), nullable=False)
+    state = db.Column(db.String(10), nullable=False)
+    zip_code = db.Column(db.Integer, nullable=False)
+    listing_id = db.Column(db.Integer, db.ForeignKey('Listing.id', nullable=False))
+
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'house_num': self.house_num,
+            'street_name': self.street_name,
+            'city': self.city,
+            'state': self.state,
+            'zip_code': self.zip_code,
+            'listing_id': self.listing_id
+        }
+
+
+class Images(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    listing_id = db.Column(db.Integer, db.ForeignKey('Listing.id', nullable=False))
+    name = db.Column(db.String(50), nullable=False)
+    path = db.Column(db.String(50), nullable=False)
+
+    def to_dict(self):
+        return {
+                'id': self.id,
+                'listing_id': self.listing_id,
+                'name': self.name,
+                'path': self.path
         }
