38b70c7f9b92851d3c36f70995d9213beb9ec931
David Silva <daverhan02@gmail.com>
2024-02-25 17:13:55 -0500

    Implement flask database migrations

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask_migrate import Migrate
 from app.routes.users import user_bp
 from app.routes.listings import listing_bp
 from app.database import db
@@ -13,11 +14,9 @@ def create_app():
     app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URI')
 
     db.init_app(app)
+    migrate = Migrate(app, db)
 
     app.register_blueprint(user_bp, url_prefix='/api/users')
     app.register_blueprint(listing_bp, url_prefix='/api/listings')
-
-    with app.app_context():
-        db.create_all()
 
     return app
