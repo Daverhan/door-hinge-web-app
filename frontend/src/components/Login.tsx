bf1980275fc3f0f42984640b408f7b82e7548b12
David Silva <daverhan02@gmail.com>
2024-03-15 11:45:46 -0400

    Update login form

--- frontend/src/components/Login.tsx
+++ frontend/src/components/Login.tsx
@@ -25,22 +25,31 @@ function Login() {
           <form className="w-full max-w-lg py-3 px-5 bg-gray-100 rounded ">
             <div className="flex flex-wrap -mx-3 mb-6å">
               <div className="w-full px-3">
-                <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">
+                <label
+                  htmlFor="username"
+                  className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+                >
                   Username
                 </label>
                 <input
                   className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
-                  id="grid-password"
+                  id="username"
+                  name="username"
                   type="username"
+                  autoComplete="username"
                 ></input>
               </div>
               <div className="w-full px-3">
-                <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">
+                <label
+                  htmlFor="password"
+                  className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+                >
                   Password
                 </label>
                 <input
                   className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
-                  id="grid-password"
+                  id="password"
+                  name="password"
                   type="password"
                 ></input>
               </div>
