a21f5e3db8046946f2cdcc751067c87a349535e8
David Silva <daverhan02@gmail.com>
2024-04-06 21:51:46 -0400

    Implement RBAC for the current state of the app

--- backend/app/routes/users.py
+++ backend/app/routes/users.py
@@ -1,7 +1,7 @@
 from flask import Blueprint, jsonify, request, session, make_response
 from app.models.user import User, Listing, Chat, Message, user_chat_association, user_favorited_listing_association, user_passed_listing_association
 from app.extensions import db, bcrypt
-from app.rbac_utilities import create_mysql_user
+from app.rbac_utilities import create_mysql_user, safe_db_connection
 
 user_bp = Blueprint('user', __name__)
 
@@ -13,9 +13,18 @@ def get_current_user():
     if not user_id:
         return jsonify({'error': 'Unauthorized'}), 401
 
-    user = User.query.filter_by(id=user_id).first()
+    with safe_db_connection(session.get('username'), session.get('password')) as user_db_session:
+        user = user_db_session.query(User).filter_by(id=user_id).first()
 
     return jsonify({'id': user.id, 'username': user.username, 'first_name': user.first_name, 'last_name': user.last_name})
+
+
+'''
+IMPORTANT:
+THIS HEADER DENOTES THAT THE FOLLOWING API ROUTE MEETS ONE OF THE FOLLOWING CRITERIA:
+- API ROUTE IS NEVER USED IN THE CLIENT-SIDE APPLICATION
+- API ROUTE NEEDS RBAC IMPLEMENTED IN IT IF NECESSARY (A USER DB CONNECTION PERFORMING ACTIONS ON THEIR BEHALF, NOT THE ADMIN DB CONNECTION)
+'''
 
 
 @user_bp.route('', methods=['GET'])
@@ -24,6 +33,14 @@ def get_users():
     users_data = [user.to_dict() for user in users]
 
     return jsonify(users_data)
+
+
+'''
+IMPORTANT:
+THIS HEADER DENOTES THAT THE FOLLOWING API ROUTE MEETS ONE OF THE FOLLOWING CRITERIA:
+- API ROUTE IS NEVER USED IN THE CLIENT-SIDE APPLICATION
+- API ROUTE NEEDS RBAC IMPLEMENTED IN IT IF NECESSARY (A USER DB CONNECTION PERFORMING ACTIONS ON THEIR BEHALF, NOT THE ADMIN DB CONNECTION)
+'''
 
 
 @user_bp.route('<int:user_id>', methods=['GET'])
@@ -60,6 +77,8 @@ def login_user():
         return jsonify({'error': 'Invalid username or password'}), 401
 
     session['user_id'] = user.id
+    session['username'] = user.username
+    session['password'] = user.password
 
     return jsonify({'message': 'Successfully logged in', 'id': user.id, 'username': user.username}), 200
 
@@ -78,24 +97,33 @@ def register_user():
         if user_exists:
             return jsonify({'error': 'A user already exists with the provided username or email'}), 409
 
-        plaintext_password = user_json['password']
-
         user_json['password'] = bcrypt.generate_password_hash(
             user_json['password'])
 
         user_data = {field: user_json[field] for field in required_fields}
         user = User(**user_data)
 
-        create_mysql_user(user_json['username'], plaintext_password, 'user')
+        create_mysql_user(
+            user_json['username'], user_json['password'].decode('utf-8'), 'user')
 
         db.session.add(user)
         db.session.commit()
 
         session['user_id'] = user.id
+        session['username'] = user.username
+        session['password'] = user.password
 
         return jsonify({'message': 'User created successfully', **user.to_dict()}), 200
 
     return jsonify({'error': 'Missing required fields'}), 400
+
+
+'''
+IMPORTANT:
+THIS HEADER DENOTES THAT THE FOLLOWING API ROUTE MEETS ONE OF THE FOLLOWING CRITERIA:
+- API ROUTE IS NEVER USED IN THE CLIENT-SIDE APPLICATION
+- API ROUTE NEEDS RBAC IMPLEMENTED IN IT IF NECESSARY (A USER DB CONNECTION PERFORMING ACTIONS ON THEIR BEHALF, NOT THE ADMIN DB CONNECTION)
+'''
 
 
 @user_bp.route('<int:user_id>', methods=['PUT'])
@@ -121,6 +149,14 @@ def update_user(user_id):
     return jsonify({'message': 'User updated successfully', **user.to_dict()}), 200
 
 
+'''
+IMPORTANT:
+THIS HEADER DENOTES THAT THE FOLLOWING API ROUTE MEETS ONE OF THE FOLLOWING CRITERIA:
+- API ROUTE IS NEVER USED IN THE CLIENT-SIDE APPLICATION
+- API ROUTE NEEDS RBAC IMPLEMENTED IN IT IF NECESSARY (A USER DB CONNECTION PERFORMING ACTIONS ON THEIR BEHALF, NOT THE ADMIN DB CONNECTION)
+'''
+
+
 @user_bp.route('<int:user_id>', methods=['DELETE'])
 def delete_user(user_id):
     user = User.query.get(user_id)
@@ -134,6 +170,14 @@ def delete_user(user_id):
         return jsonify({'error': 'User not found'}), 404
 
 
+'''
+IMPORTANT:
+THIS HEADER DENOTES THAT THE FOLLOWING API ROUTE MEETS ONE OF THE FOLLOWING CRITERIA:
+- API ROUTE IS NEVER USED IN THE CLIENT-SIDE APPLICATION
+- API ROUTE NEEDS RBAC IMPLEMENTED IN IT IF NECESSARY (A USER DB CONNECTION PERFORMING ACTIONS ON THEIR BEHALF, NOT THE ADMIN DB CONNECTION)
+'''
+
+
 @user_bp.route('<int:user_id>/chats', methods=['GET'])
 def get_user_chats(user_id):
     user = User.query.get(user_id)
@@ -142,6 +186,14 @@ def get_user_chats(user_id):
         return jsonify({'error': 'User not found'}), 404
 
     return jsonify([chat.to_dict() for chat in user.chats])
+
+
+'''
+IMPORTANT:
+THIS HEADER DENOTES THAT THE FOLLOWING API ROUTE MEETS ONE OF THE FOLLOWING CRITERIA:
+- API ROUTE IS NEVER USED IN THE CLIENT-SIDE APPLICATION
+- API ROUTE NEEDS RBAC IMPLEMENTED IN IT IF NECESSARY (A USER DB CONNECTION PERFORMING ACTIONS ON THEIR BEHALF, NOT THE ADMIN DB CONNECTION)
+'''
 
 
 @user_bp.route('<int:user_id>/chats/<int:chat_id>', methods=['GET'])
@@ -167,6 +219,14 @@ def get_a_user_chat(user_id, chat_id):
     return jsonify(user_chat.to_dict())
 
 
+'''
+IMPORTANT:
+THIS HEADER DENOTES THAT THE FOLLOWING API ROUTE MEETS ONE OF THE FOLLOWING CRITERIA:
+- API ROUTE IS NEVER USED IN THE CLIENT-SIDE APPLICATION
+- API ROUTE NEEDS RBAC IMPLEMENTED IN IT IF NECESSARY (A USER DB CONNECTION PERFORMING ACTIONS ON THEIR BEHALF, NOT THE ADMIN DB CONNECTION)
+'''
+
+
 @user_bp.route('<int:user_id>/chats', methods=['POST'])
 def create_user_chat(user_id):
     user = User.query.get(user_id)
@@ -183,6 +243,14 @@ def create_user_chat(user_id):
     db.session.commit()
 
     return jsonify({'message': 'Chat created successfully', 'chat_id': chat.id, 'user_id': user.id}), 201
+
+
+'''
+IMPORTANT:
+THIS HEADER DENOTES THAT THE FOLLOWING API ROUTE MEETS ONE OF THE FOLLOWING CRITERIA:
+- API ROUTE IS NEVER USED IN THE CLIENT-SIDE APPLICATION
+- API ROUTE NEEDS RBAC IMPLEMENTED IN IT IF NECESSARY (A USER DB CONNECTION PERFORMING ACTIONS ON THEIR BEHALF, NOT THE ADMIN DB CONNECTION)
+'''
 
 
 @user_bp.route('<int:user_id>/chats/<int:chat_id>', methods=['DELETE'])
@@ -211,6 +279,14 @@ def delete_user_chat(user_id, chat_id):
     db.session.commit()
 
     return jsonify({'message': 'Chat successfully removed from the user'}), 200
+
+
+'''
+IMPORTANT:
+THIS HEADER DENOTES THAT THE FOLLOWING API ROUTE MEETS ONE OF THE FOLLOWING CRITERIA:
+- API ROUTE IS NEVER USED IN THE CLIENT-SIDE APPLICATION
+- API ROUTE NEEDS RBAC IMPLEMENTED IN IT IF NECESSARY (A USER DB CONNECTION PERFORMING ACTIONS ON THEIR BEHALF, NOT THE ADMIN DB CONNECTION)
+'''
 
 
 @user_bp.route('<int:user_id>/chats/<int:chat_id>/messages', methods=['POST'])
@@ -250,29 +326,39 @@ def create_message(user_id, chat_id):
 def favorite_a_listing_for_user():
     user_id = session.get('user_id')
 
-    user = User.query.get(user_id)
+    with safe_db_connection(session.get('username'), session.get('password')) as user_db_session:
+        user = user_db_session.query(User).get(user_id)
 
-    listing_id_json = request.get_json()
+        listing_id_json = request.get_json()
 
-    if not user:
-        return jsonify({'error': 'User not found'}), 404
+        if not user:
+            return jsonify({'error': 'User not found'}), 404
 
-    if 'listing_id' not in listing_id_json:
-        return jsonify({'error': 'Missing listing id'}), 404
+        if 'listing_id' not in listing_id_json:
+            return jsonify({'error': 'Missing listing id'}), 404
 
-    listing_id = listing_id_json['listing_id']
+        listing_id = listing_id_json['listing_id']
 
-    listing = Listing.query.get(listing_id)
+        listing = user_db_session.query(Listing).get(listing_id)
 
-    if not listing:
-        return jsonify({'error': 'Listing not found'}), 404
+        if not listing:
+            return jsonify({'error': 'Listing not found'}), 404
 
-    new_association = {'user_id': user_id, 'listing_id': listing_id}
-    db.session.execute(
-        user_favorited_listing_association.insert().values(new_association))
-    db.session.commit()
+        new_association = {'user_id': user_id, 'listing_id': listing_id}
+        user_db_session.execute(
+            user_favorited_listing_association.insert().values(new_association))
+
+        user_db_session.commit()
 
     return jsonify({'message': 'Listing successfully favorited for the user'}), 201
+
+
+'''
+IMPORTANT:
+THIS HEADER DENOTES THAT THE FOLLOWING API ROUTE MEETS ONE OF THE FOLLOWING CRITERIA:
+- API ROUTE IS NEVER USED IN THE CLIENT-SIDE APPLICATION
+- API ROUTE NEEDS RBAC IMPLEMENTED IN IT IF NECESSARY (A USER DB CONNECTION PERFORMING ACTIONS ON THEIR BEHALF, NOT THE ADMIN DB CONNECTION)
+'''
 
 
 @user_bp.route('<int:user_id>/favorite-listings/<int:listing_id>', methods=['DELETE'])
@@ -307,26 +393,28 @@ def unfavorite_a_listing_from_user(user_id, listing_id):
 def pass_a_listing_for_user():
     user_id = session.get('user_id')
 
-    user = User.query.get(user_id)
+    with safe_db_connection(session.get('username'), session.get('password')) as user_db_session:
+        user = user_db_session.query(User).get(user_id)
 
-    listing_id_json = request.get_json()
+        listing_id_json = request.get_json()
 
-    if not user:
-        return jsonify({'error': 'User not found'}), 404
+        if not user:
+            return jsonify({'error': 'User not found'}), 404
 
-    if 'listing_id' not in listing_id_json:
-        return jsonify({'error': 'Missing listing id'}), 404
+        if 'listing_id' not in listing_id_json:
+            return jsonify({'error': 'Missing listing id'}), 404
 
-    listing_id = listing_id_json['listing_id']
+        listing_id = listing_id_json['listing_id']
 
-    listing = Listing.query.get(listing_id)
+        listing = user_db_session.query(Listing).get(listing_id)
 
-    if not listing:
-        return jsonify({'error': 'Listing not found'}), 404
+        if not listing:
+            return jsonify({'error': 'Listing not found'}), 404
 
-    new_association = {'user_id': user_id, 'listing_id': listing_id}
-    db.session.execute(
-        user_passed_listing_association.insert().values(new_association))
-    db.session.commit()
+        new_association = {'user_id': user_id, 'listing_id': listing_id}
+        user_db_session.execute(
+            user_passed_listing_association.insert().values(new_association))
+
+        user_db_session.commit()
 
     return jsonify({'message': 'Listing successfully passed for the user'}), 201
