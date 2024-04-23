d07f40210573f9bdf8e77d8b95dd32359efc7036
madelinewhitt <madelinewhitton@MacBook-Air-10.local>
2024-04-22 22:09:38 -0400

    Added a route for Updating Users

--- backend/app/routes/users.py
+++ backend/app/routes/users.py
@@ -138,6 +138,27 @@ def register_user():
 
     return jsonify({'error': 'Missing required fields'}), 400
 
+@user_bp.route('<int:user_id>', methods=['PUT'])
+def update_listing(user_id):
+    user_json = request.get_json()
+    user = User.query.get(user_id)
+
+    updatable_fields = ['username', 'password',
+                        'first_name', 'last_name']
+
+    if not user:
+        return jsonify({'error': 'User not found'}), 404
+
+    if not any(field in user_json for field in updatable_fields):
+        return jsonify({'message': 'At least one updatable field must be provided'})
+
+    for field in updatable_fields:
+        if field in user_json:
+            setattr(user, field, user_json[field])
+
+    db.session.commit()
+
+    return jsonify({'message': 'User updated successfully', **user.to_dict()}), 200
 
 '''
 IMPORTANT:
