6f115161e659a537908227313656d316ac379777
madelinewhitt <madelinewhitton@MacBook-Air-10.local>
2024-04-24 01:38:47 -0400

    Added password front end and backend functionality

--- frontend/src/components/ResetPassword.tsx
+++ frontend/src/components/ResetPassword.tsx
@@ -1,19 +1,73 @@
-import { useRef } from "react";
+import { useRef, useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 function ResetPassword() {
     const formRef = useRef<HTMLFormElement>(null);
+    const [error409Flag, setError409Flag] = useState(false);
+    const [emptyInputFlag, setEmptyInputFlag] = useState(false);
+    const [serverErrorFlag, setServerErrorFlag] = useState(false);
+    const [dontMatchFlag, setDontMatchFlag] = useState(false);
+
+    const MAX_PASSWORD_LENGTH = 100;
+
 
     const navigate = useNavigate();
-
-    const navigateTo = (path: string) => () => {
-        navigate(path);
-    };
 
     const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
         if (!formRef.current) {
             return;
         }
+
+        const formData = new FormData(formRef.current);
+
+        const formValues = {
+            password: formData.get("password"),
+            duplicate: formData.get("duplicate")
+        };
+
+        let emptyInputFields = false;
+
+        Object.values(formValues).forEach((value) => {
+            if (!value) {
+                setEmptyInputFlag(true);
+                emptyInputFields = true;
+            }
+        });
+
+        if (emptyInputFields) {
+            return;
+        } else {
+            setEmptyInputFlag(false);
+        }
+
+        let dontMatch = false;
+
+        if (formValues.password !== formValues.duplicate) {
+            setDontMatchFlag(true);
+            dontMatch = true;
+        }
+
+        if (dontMatch) {
+            return;
+        } else {
+            setDontMatchFlag(false);
+        }
+
+        fetch("/api/users/resetpassword", {
+            method: "PUT",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify(formValues),
+        }).then((response) => {
+            if (response.status === 409) {
+                setError409Flag(true);
+            }
+            if (response.ok) {
+                navigate("/profile");
+            }
+            else {
+                setServerErrorFlag(true);
+            }
+        });
     };
 
     return (
@@ -45,6 +99,8 @@ function ResetPassword() {
                                 <input
                                     className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
                                     type="password"
+                                    id="password"
+                                    name="password"
                                 ></input>
                             </div>
                             <div className="w-full px-3">
@@ -57,11 +113,25 @@ function ResetPassword() {
                                 <input
                                     className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
                                     type="password"
+                                    id="duplicate"
+                                    name="duplicate"
                                 ></input>
                             </div>
                         </div>
+                        {/* Flags here */}
+                        {emptyInputFlag ? (
+                            <p className="text-2xl text-center mb-2 text-red-500">
+                                Error: Fields cannot empty
+                            </p>
+                        ) : null}
+                        {dontMatchFlag ? (
+                            <p className="text-2xl text-center mb-2 text-red-500">
+                                Error: Passwords don't match
+                            </p>
+                        ) : null}
                         <button className="flex justify-center bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded w-full"
-                            onClick={navigateTo("/Profile")}>
+                        // onClick={navigateTo("/Profile")}
+                        >
                             Save Changes
                         </button>
                         <div className="py-1"></div>
