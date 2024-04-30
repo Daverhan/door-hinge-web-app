cb5522e9f567f057ec0d8e1e314d838e4b3c2169
Silk-OT <omerntuzun@gmail.com>
2024-04-30 16:28:29 -0400

    Mobile Support 0.5 Being Added

--- frontend/src/components/Messages.tsx
+++ frontend/src/components/Messages.tsx
@@ -101,8 +101,8 @@ function Messaging() {
 
   return (
     <section className="bg-blue-100 h-screen pt-16">
-      <div className="grid grid-cols-6 divide-x h-screen-adjusted">
-        <div className="grid grid-cols-1 col-start-1 col-end-3 h-screen-adjusted w-100">
+      <div className="grid grid-cols-6 h-screen-adjusted">
+        <div className="hidden lg:block grid-cols-1 col-start-1 col-end-3 h-screen-adjusted w-100">
           <div className="flex flex-col row-start-1 row-end-2 col-start-1 col-end-2 justify-items-center justify-center text-center items-center">
             {chats.map((chat, index) => (
               <div
@@ -115,13 +115,13 @@ function Messaging() {
             ))}
           </div>
         </div>
-        <div className="flex justify-evenly items-center bg-white row-span-2 col-start-3 col-end-7">
+        <div className="flex justify-evenly items-center bg-white row-span-2 col-start-1 lg:col-start-3 col-end-7">
           <div className="grid items grid-cols-2 grid-rows-[7%_4fr_10%] w-full h-full space-x-0 space-y-0">
             <header className="flex bg-blue-gray-50 col-start-1 col-end-3 justify-items-center justify-center text-center items-center mt-0">
               Messaging {currentChatUser ? `${currentChatUser}` : ""}
             </header>
             <div
-              className="h-96 flex flex-col overflow-auto row-start-2 row-end-5 col-start-1 col-end-3 border border-gray-500 shadow-lg"
+              className="flex h-96 flex-col overflow-auto row-start-2 row-end-5 col-start-1 col-end-3 border border-gray-500 shadow-lg"
               ref={messageEndRef}
             >
               {messages.map((msg, index) =>
@@ -130,39 +130,40 @@ function Messaging() {
                     <div></div>
                     <div
                       key={index}
-                      className="message border-solid border-2 border-gray-200 h-32 overflow-hidden break-words p-2 m-2 bg-white"
+                      className="message border-solid border-2 border-gray-200 overflow-hidden break-words rounded-lg p-2 m-2 bg-white"
                     >
-                      {msg.sender_name}: {msg.content} (sent at {msg.timestamp})
+                      {msg.content} <br></br> <p className="text-gray-600 text-right">Sent on {msg.timestamp}</p>
                     </div>
                   </div>
                 ) : (
                   <div className="grid grid-cols-2">
                     <div
                       key={index}
-                      className="message border-solid border-2 border-gray-200 h-32 overflow-hidden break-words p-2 m-2 bg-white"
+                      className="message border-solid border-2 border-blue-200 overflow-hidden break-words rounded-lg p-2 m-2 bg-white"
                     >
-                      {msg.sender_name}: {msg.content} (sent at {msg.timestamp})
+                      {msg.content} <br></br> <span className="text-gray-600">Sent on {msg.timestamp}</span>
                     </div>
                     <div></div>
                   </div>
                 )
               )}
             </div>
-            <footer className="flex bg-green-50 mt-auto col-start-1 col-end-3 row-start-6 justify-items-center text-center items-center">
+            <footer className="flex mt-auto col-start-1 col-end-3 row-start-6 justify-items-center text-center items-center">
               {}
               <form onSubmit={handleSubmit} className="w-full flex">
                 <input
                   type="text"
                   id="message_tag"
-                  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg block w-3/4 p-2.5 mr-2"
+                  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg block w-3/4 p-2.5 ml-2 mb-2 mr-2"
                   placeholder="Message"
                   required
                   value={messageText}
                   onChange={handleMessageChange}
+
                 />
                 <button
                   type="submit"
-                  className="text-white bg-blue-200 hover:bg-blue-500 font-medium rounded text-sm w-1/4 px-5 py-2.5 text-center"
+                  className="text-white bg-blue-200 hover:bg-blue-500 font-medium rounded text-sm w-1/4 px-5 py-2.5 mr-2 mb-2 text-center"
                 >
                   Send
                 </button>
